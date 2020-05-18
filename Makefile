e08267e672f8db52e435c390557bacb4df16b1b3
kakiang <kakiang.hk@gmail.com>
2020-05-18 10:46:55 +0000

    Add app entry point for migrating the database and running the app
    Signed-off-by: kakiang <kakiang.hk@gmail.com>

---
+++ Makefile
@@ -0,0 +1,20 @@
+clean:
+	find . -type f -name '*.pyc' -delete
+	find . -type f -name '*.log' -delete
+	rm -rf migrations
+	rm sme_financing/main/*.db
+
+upgrade-db:
+	python manage.py db migrate
+	python manage.py db upgrade
+
+init-db:
+	python manage.py db init
+
+all-db: init-db upgrade-db
+
+tests:
+	python manage.py test
+
+run:
+	python manage.py run
---
+++ manage.py
@@ -0,0 +1,57 @@
+""" application entry point """
+
+import os
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from sme_financing.main import create_app, db
+from sme_financing.main.api_v1 import blueprint as api_v1
+from sme_financing.main.models import (
+    client,
+    document,
+    fund_disbursement,
+    funding_application,
+    funding_criteria,
+    funding_detail,
+    funding_project,
+    investor,
+    project_milestone,
+    sme,
+    user,
+)
+
+app = create_app(os.getenv("FLASK_ENV") or "development")
+
+app.register_blueprint(api_v1)
+
+app.app_context().push()
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command("db", MigrateCommand)
+
+
+@manager.command
+def run():
+    """Runs app from the command line."""
+    print("run command!!!!!!!!!!!!!!!!!!!!")
+    app.run()
+    print("exit run command!!!!!!!!!!!!!!!!!!!!")
+
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover("tests", pattern="test*.py")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+if __name__ == "__main__":
+    manager.run()
