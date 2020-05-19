934a7909ff7f8d123537eb355241eccefdf65e51
kakiang <kakiang.hk@gmail.com>
2020-05-19 06:08:12 +0000

    Fix 'Mixed content blocked' issue

--- sme_financing/main/__init__.py
+++ sme_financing/main/__init__.py
@@ -1,9 +1,9 @@
 """App factory file."""
 
-# from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from .config import config
 
@@ -14,7 +14,7 @@ flask_bcrypt = Bcrypt()
 def create_app(config_name):
     """Application factory."""
     app = Flask(__name__)
-    # app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     app.config.from_object(config[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
