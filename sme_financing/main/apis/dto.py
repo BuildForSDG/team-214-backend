7ba3f936b6538bd093d1f1b7d1864375adcb85d3
akola <akoladenis97@gmail.com>
2020-06-03 00:39:44 +0000

    Updates on Funding Criteria api

--- sme_financing/main/apis/dto.py
+++ sme_financing/main/apis/dto.py
@@ -154,11 +154,16 @@ class FundingApplicationDTO:
 
 
 class FundingCriteriaDTO:
-    funding_criteria_api = Namespace("funding criteria",description="Funding criteria related operations")
-    funding_criteria =funding_criteria_api.model("funding_criteria",{
-        "name": fields.String(required =True,description = "Funding Criteria name"),
-        "description": fields.String(required = True,description = " Description of funding criteria"),
-        "investor_id": fields.Integer(required =True),
-
-
-    },)    
+    funding_criteria_api = Namespace(
+        "funding criteria", description="Funding criteria related operations"
+    )
+    funding_criteria = funding_criteria_api.model(
+        "funding_criteria",
+        {
+            "name": fields.String(required=True, description="Funding Criteria name"),
+            "description": fields.String(
+                required=True, description=" Description of funding criteria"
+            ),
+            "investor_id": fields.Integer(required=True),
+        },
+    )
