360c78ad633356f96460f6f0f9de3037ee72b10e
akola <akoladenis97@gmail.com>
2020-06-03 01:22:12 +0000

    Funding criteria endpoints fixed

--- sme_financing/main/apis/funding_criteria_api.py
+++ sme_financing/main/apis/funding_criteria_api.py
@@ -27,14 +27,14 @@ class fundingCriteriaResource(Resource):
     @api.response(201, "Funding criteria successfully created") 
     def post(self):
         '''
-        Create a new fundfing criteria
+        Create a new funding criteria
         '''
         data =resquest.json
         return save_funding_criteria(data=data)
 
 
 
-api.route("/funding_criteria/<int:funding_criteria_id>")
+@api.route("/funding_criteria/<int:funding_criteria_id>")
 @api.param("funding_criteria_id", "The ID of the Funding criteria to process")
 @api.response(HTTPStatus.NOT_FOUND, "Funding criteria  not found")
 class FundingCriteriaById(Resource):
@@ -57,7 +57,7 @@ class FundingCriteriaById(Resource):
     @api.response(HTTPStatus.BAD_REQUEST, "Can't delete the Funding Criteria")
     def delete(self,funding_criteria_id):
         '''
-        Retrieve the funding criteria by the given Id to delete it
+       Delete the funding criteria by the given Id to delete it
         ''' 
         funding_criteria = get_funding_criteria_by_id(funding_criteria_id)
         if not funding_criteria:
@@ -69,10 +69,10 @@ class FundingCriteriaById(Resource):
 
 
 
-    @api.doc("Update a Funding Criteria")
+    @api.doc("Update a funding Criteria")
     @api.expect(_funding_criteria, validate=True)
-    @api.response(HTTPStatus.CREATED, "Funding Criteria  successfully updated")
-    @api.response(HTTPStatus.BAD_REQUEST, "Can't update the Funding Application")
+    @api.response(HTTPStatus.CREATED, "Funding criteria  successfully updated")
+    @api.response(HTTPStatus.BAD_REQUEST, "Can't update the funding criteria")
     @api.response(HTTPStatus.NOT_FOUND, "Investor  specified doesn't exist")
     def patch(self, funding_criteria_id):
         """Update a Funding Application."""
