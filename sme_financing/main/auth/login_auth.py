fc4e57e2193acbcbca57076c4c7f561f865bcc04
akola <akoladenis97@gmail.com>
2020-06-07 01:55:03 +0000

    Update on authenication

--- sme_financing/main/auth/login_auth.py
+++ sme_financing/main/auth/login_auth.py
@@ -5,14 +5,14 @@ import requests
 from firebase_admin import auth
 from firebase_admin.exceptions import FirebaseError
 
-FIREBASE_WEB_API_KEY = "AIzaSyB5XpnbwDmwRu5c1rv5zI3EjTfYKFq_Mak"
+FIREBASE_WEB_API_KEY = os.environ.get("FIREBASE_WEB_API_KEY")
 
 rest_api_url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
 
 
 def login_with_email_and_password(data, return_secure_token: bool = True):
     user_email = data["email"]
-    user_password = data["password"]
+
     payload = json.dumps(
         {
             "email": data["email"],
@@ -41,16 +41,36 @@ def verify_user(token):
             }
             return response_object, 202
         except FirebaseError as error:
-            response_object = {"status": "failure", "message": "Invalid token"}
+            response_object = {"status": "failure", "message": str(error)}
             return response_object, 502
     else:
         pass
 
 
-# def forget_password(email:str):
-#     '''
+def forget_password(email: str):
+    """
+    It takes an email and sent a reset password email to the user
 
-#     '''
+    """
+    user = auth.get_user_by_email(email=email)
+    if user:
+
+        try:
+            auth.generate_password_reset_link(email=email)
+            response_object = {
+                "status": "success",
+                "message": "Link sent!",
+            }
+            return response_object, 202
+        except FirebaseError as error:
+            response_object = {"status": "failed", "message": str(error)}
+            return response_object, 502
+    response_object = {"status": "failed", "message": "Email does not exist!"}
+    return response_object, 500
+
+
+def reset_password(data):
+    pass
 
 
 if __name__ == "__main__":
