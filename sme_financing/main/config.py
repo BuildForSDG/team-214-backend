4a9adaf43bccf72133fe8621d1742a50cab46fa4
kakiang <kakiang.hk@gmail.com>
2020-06-16 09:15:05 +0000

    Add env testing.

--- sme_financing/main/config.py
+++ sme_financing/main/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-env_path = os.path.join(basedir, "../../.env")
+env_path = os.path.join(basedir, "../../.env.testing")
 load_dotenv(dotenv_path=env_path)
 
 
